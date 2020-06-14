99df07dfff9c4a5c7f0fb9e24bba2a184f446984
nevinbaiju <nevinbaiju@gmail.com>
2020-06-14 21:25:38 +0530

    Initial development of prototype

---
+++ main/game_objects/environment.go
@@ -0,0 +1,33 @@
+components {
+  id: "spawn_enemy"
+  component: "/main/scripts/spawn_enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemy_fish_spawn"
+  type: "factory"
+  data: "prototype: \"/main/game_objects/enemy_fish.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
